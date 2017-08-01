bf4b536570a85e9779aa5318676b4b203dbbe39b
gaopeng <service@lekee.cc>
2017-08-01 12:51:08 +0800

    instll monolog

--- index.php
+++ index.php
@@ -4,5 +4,13 @@
 require 'vendor/autoload.php';
 use App\leke\demo\Greeting;
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 $greet = new Greeting();
 print $greet->hello();
+
+$log = new Logger('leke');
+$log->pushHandler(new StreamHandler('app/log/demo.log', Logger::DEBUG));
+
+$log->info('hello ~ logger.');
